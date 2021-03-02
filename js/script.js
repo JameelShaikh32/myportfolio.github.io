a30ca24d850fbbf7ff477021d130d1d6597ffc07
Jameel Shaikh <42856910+JameelShaikh32@users.noreply.github.com>
2021-03-02 23:06:51 +0530

    upadate files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-alert("If you are opening this website on a mobile phone switch on desktop mode on your browser.")
+window.alert("If you are opening this website on a mobile phone switch on desktop mode on your browser.");
