96af622d53e9c77f8c3294b5a76a3122aa70085c
Jameel Shaikh <42856910+JameelShaikh32@users.noreply.github.com>
2021-03-23 15:47:40 +0530

    Updated Files

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ var nav = document.getElementById("navbars");
 
 function openMenu() {
     nav.style.right = "0px";
+    nav.style.display = "block";
+    document.getElementById("menu-btn").style.display = "none";
 }
 
 function closeMenu() {
     nav.style.right = "-200px";
+    nav.style.display = "none";
+    document.getElementById("menu-btn").style.display = "block";
 }
