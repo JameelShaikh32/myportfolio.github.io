4a6acbc6be965cfc2ab3d250f55f1a7d85092f52
Jameel Shaikh <42856910+JameelShaikh32@users.noreply.github.com>
2021-06-28 22:48:50 +0530

    Added Resume Page

---
+++ js/resume.js
@@ -0,0 +1,23 @@
+$("#cert1").hover(function() {
+    $("#cyber").css("display", "block");
+}, function() {
+    $("#cyber").css("display", "none");
+});
+
+$("#cert2").hover(function() {
+    $("#python").css("display", "block");
+}, function() {
+    $("#python").css("display", "none");
+});
+
+$("#achv1").hover(function() {
+    $("#cyberKart").css("display", "block");
+}, function() {
+    $("#cyberKart").css("display", "none");
+});
+
+$("#achv2").hover(function() {
+    $("#oddballs").css("display", "block");
+}, function() {
+    $("#oddballs").css("display", "none");
+});
